5004929eeffae5337087da26faff66f330eff031
David Tolmie <dave@bodhitech.com>
2015-11-03 15:57:32 -0800

    Changed unordered lists to a single table.

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-function CookieStore(storeLoc, minPersons, maxPersons, avgCookiesPerCust) {
+var times = ['10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm'];
 
-  this.hours = 8; // if you update hours you need to manually update the idNames variable and times variable
+function CookieStore(storeLoc, minPersons, maxPersons, avgCookiesPerCust) {
   this.storeLoc = storeLoc;
   this.minPersons = minPersons;
   this.maxPersons = maxPersons;
   this.avgCookiesPerCust = avgCookiesPerCust;
   this.avgDailyTotal = 0;
   this.avgCookiesPerHr = [];
-
 
   this.customersPerHr = function(max, min) {
     return Math.random() * (max - min + 1) + min;
@@ -18,52 +17,45 @@ function CookieStore(storeLoc, minPersons, maxPersons, avgCookiesPerCust) {
   };
 
   this.pushToArray = function() {
-    for (var i = 0; i < this.hours; i++) {
+    for (var i = 0; i < times.length; i++) {
     this.avgCookiesPerHr.push(Math.floor(this.getHourlyTotal())); // pushes to array
     }
     return this.avgCookiesPerHr;
   };
 
   this.getDailyTotal = function() {
-    for (var i = 0; i < this.hours; i++) {
+    for (var i = 0; i < times.length; i++) {
       this.avgDailyTotal += this.avgCookiesPerHr[i];
     }
     return this.avgDailyTotal;
   };
 
   this.display = function() {
-    var newListItem = 0;
-    var newText = '';
-    var times = ['10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm'];
-    var idNames = ['10amPike', '11amPike', '12pmPike', '1pmPike', '2pmPike', '3pmPike', '4pmPike', '5pmPike'];
-    var liItems = this.pushToArray();
-    console.log(liItems);
+    this.pushToArray();
 
-    var store = document.createElement('h4');
-    store.innerHTML = this.storeLoc;
-    document.body.appendChild(store);
+    var tableRow = document.createElement('tr');
+    table.appendChild(tableRow);
 
-    var newUlist = document.createElement('ul');
-    newUlist.setAttribute('class', 'listItem');
-    document.body.appendChild(newUlist);
+    var tableData = document.createElement('td');
+    var newText = document.createTextNode(this.storeLoc);
+    tableData.appendChild(newText);
+    tableRow.appendChild(tableData);
+    tableData.setAttribute("class", "tableData");
 
-    for (var i = 0; i < this.hours; i++) {
-
-      newListItem = document.createElement('li');
-      newText = document.createTextNode(times[i] + ' ' + liItems[i]);
-      newListItem.appendChild(newText);
-      newUlist.appendChild(newListItem);
-      newListItem.setAttribute('id', idNames[i]);
-      newListItem.setAttribute('class', 'items');
+    for (var i = 0; i < times.length; i++) {
+      var tableData = document.createElement('td');
+      var newText = document.createTextNode(this.avgCookiesPerHr[i]);
+      tableData.appendChild(newText);
+      tableRow.appendChild(tableData);
+      tableData.setAttribute("class", "tableData");
     }
 
     var sold = this.getDailyTotal();
-    newListItem = document.createElement('li');
-    newText = document.createTextNode('Total ' + sold);
-    newListItem.appendChild(newText);
-    newUlist.appendChild(newListItem).setAttribute('id', 'pikeDaily');
-    newListItem.setAttribute('class', 'items');
-
+    var tableData = document.createElement('td');
+    var newText = document.createTextNode(sold);
+    tableData.appendChild(newText);
+    tableRow.appendChild(tableData);
+    tableData.setAttribute("class", "tableData");
   };
 
 } //end store constructor
@@ -73,6 +65,32 @@ var seaTac = new CookieStore('SeaTac Airport', 6, 44, 1.2);
 var southCenter = new CookieStore('South Center Mall', 11, 38, 1.9);
 var bellSquare = new CookieStore('Bellevue Square', 20, 48, 3.3);
 var alki = new CookieStore('Alki', 3, 24, 2.6);
+
+var table = document.createElement('table');
+document.body.appendChild(table); //Makes table by appending the above
+
+var tableRow = document.createElement('tr');
+table.appendChild(tableRow);
+
+var tableHeader = document.createElement('th');
+var newText = document.createTextNode(' ');
+tableHeader.appendChild(newText);
+tableRow.appendChild(tableHeader);
+tableHeader.setAttribute("class", "tableHeaders");
+
+for (var i = 0; i < times.length; i++) {
+  var tableHeader = document.createElement('th');
+  var newText = document.createTextNode(times[i]);
+  tableHeader.appendChild(newText);
+  tableRow.appendChild(tableHeader);
+  tableHeader.setAttribute("class", "tableHeaders");
+}
+
+var tableHeader = document.createElement('th');
+var newText = document.createTextNode('Total');
+tableHeader.appendChild(newText);
+tableRow.appendChild(tableHeader);
+tableHeader.setAttribute("class", "tableHeaders");
 
 pikePlace.display();
 seaTac.display();
